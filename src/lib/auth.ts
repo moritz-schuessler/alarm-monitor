171496ae46f4ef5c6e8c3a03442941d277e7370b
Moritz Schüßler <mail@moritzschuessler.de>
2025-06-24 01:10:57 +0200

    feat(auth): add basic authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+import { createSession, decrypt, deleteSession } from "./session";
+import { getFiretruck } from "@/data/getFiretruck";
+
+const auth = async () => {
+  const cookie = (await cookies()).get("session")?.value;
+  return await decrypt(cookie);
+};
+
+const signIn = async (radioIdentification: string) => {
+  const firetruck = await getFiretruck(radioIdentification);
+
+  if (!firetruck) {
+    return {
+      message: "An error occurred while authenticating.",
+    };
+  }
+
+  await createSession(firetruck.id);
+};
+
+const signOut = async () => {
+  await deleteSession();
+};
+
+export { auth, signIn, signOut };
