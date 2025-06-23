426a884de536c4aea3883f9d9db754fa5bd10de6
Moritz Schüßler <mail@moritzschuessler.de>
2025-06-23 02:42:20 +0200

    feat: add station-schema and add references

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -1,8 +1,28 @@
+import { relations } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const firetruckTable = sqliteTable("firetruck", {
+export const stations = sqliteTable("stations", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  radioIdentification: text("radio-id").notNull(),
+  name: text("name").notNull(),
 });
+
+export const stationsRelations = relations(stations, ({ many }) => ({
+  posts: many(firetrucks),
+}));
+
+export const firetrucks = sqliteTable("firetrucks", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  radioIdentification: text("radio_id").notNull(),
+  stationId: text("station_id"),
+});
+
+export const firetrucksRelations = relations(firetrucks, ({ one }) => ({
+  station: one(stations, {
+    fields: [firetrucks.stationId],
+    references: [stations.id],
+  }),
+}));
