8d352a92f1469060792e09cb01951fee1d178f39
Moritz Schüßler <mail@moritzschuessler.de>
2025-06-24 19:58:24 +0200

    feat: add selection of firetrucks for sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,58 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import getStations from "@/data/stations/getStations";
 import { signIn } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-const SelectUserPage = () => {
+const SelectUserPage = async () => {
   const signInAction = async (formData: FormData) => {
     "use server";
     const radioIdentification = formData.get("radioIdentification") as string;
 
-    signIn(radioIdentification);
+    await signIn(radioIdentification);
 
     redirect("/");
   };
 
+  const stations = await getStations();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <form action={signInAction}>
-          <input type="text" name="radioIdentification" />
-          <button type="submit">Sign In</button>
-        </form>
-      </main>
-    </div>
+    <main className="grid place-items-center h-screen w-screen">
+      <form action={signInAction} className="flex gap-4 justify-center">
+        <Select name="radioIdentification">
+          <SelectTrigger className=" w-[40ch]">
+            <SelectValue placeholder="Feuerwehrauto" />
+          </SelectTrigger>
+          <SelectContent>
+            {stations.map((station) => {
+              return (
+                <SelectGroup key={station.id}>
+                  <SelectLabel>{station.name}</SelectLabel>
+                  {station.firetrucks.map((firetruck) => {
+                    return (
+                      <SelectItem
+                        value={firetruck.radioIdentification}
+                        key={firetruck.id}
+                      >
+                        {firetruck.radioIdentification}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectGroup>
+              );
+            })}
+          </SelectContent>
+        </Select>
+        <button type="submit">Sign in</button>
+      </form>
+    </main>
   );
 };
 
---
+++ src/data/stations/getStations.ts
@@ -0,0 +1,13 @@
+import db from "..";
+
+const getStations = async () => {
+  const result = await db.query.stations.findMany({
+    with: {
+      firetrucks: true,
+    },
+  });
+
+  return result;
+};
+
+export default getStations;
