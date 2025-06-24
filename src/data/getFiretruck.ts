1dbe46a3e81744f7e834b05d21e79d5fe81dc0c7
Moritz Schüßler <mail@moritzschuessler.de>
2025-06-24 20:10:24 +0200

    chore: remove unused imports and console.log

--- src/data/getFiretruck.ts
+++ src/data/getFiretruck.ts
@@ -1,15 +1,10 @@
-import { eq } from "drizzle-orm";
 import db from "./index";
-import { Firetruck, firetruckTable } from "./schema";
-import { log } from "console";
 
 const getFiretruck = async (radioIdentification: string) => {
   const response = await db.query.firetrucks.findFirst({
     where: (firetrucks, { eq }) =>
       eq(firetrucks.radioIdentification, radioIdentification),
   });
-
-  log("fireteruck", radioIdentification);
 
   return response;
 };
