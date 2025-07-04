ef7b0be553acb593e0caa1bf0fa809d6b4b4d581
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-04 18:04:27 +0200

    feat: add QueryClientProvider

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
