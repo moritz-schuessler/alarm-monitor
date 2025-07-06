4888efd11bbcc88b310d34b83500fbc0aee209ab
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-07 00:27:15 +0200

    feat: cache active-incident in useMe-Hook

---
+++ src/app/(app)/incident/assigned-units.tsx
@@ -0,0 +1,9 @@
+const AssignedUnits = () => {
+  return (
+    <div className="w-full divide-y-1 divide-border border-1 border-border rounded-lg text-xl col-span-1">
+      Einheiten...
+    </div>
+  );
+};
+
+export default AssignedUnits;
--- src/hooks/use-me.ts
+++ src/hooks/use-me.ts
@@ -1,18 +1,28 @@
 import { Firetrucks } from "@/data/schema";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 interface Response {
   firetruck: Firetrucks;
 }
 
 const useMe = () => {
-  return useQuery({
+  const queryClient = useQueryClient();
+  const response = useQuery({
     queryKey: ["firetruck/me"],
     queryFn: async () => {
       const response = await fetch("/api/me");
       return (await response.json()) as Response;
     },
   });
+
+  if (response.data) {
+    queryClient.setQueryData(
+      ["active-incident"],
+      () => response.data.firetruck.activeIncident,
+    );
+  }
+
+  return { ...response };
 };
 
 export default useMe;
