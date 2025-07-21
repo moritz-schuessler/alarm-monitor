0ce6381d3aae6370fad40a14d2ab2b837b70124e
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-22 00:11:46 +0200

    refactor: remove unused endpoint

--- apps/web/src/app/api/me/incident/route.ts
+++ apps/web/src/app/api/me/incident/route.ts
@@ -1,26 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { auth } from "@/lib/auth";
-import firetruckService from "@/data/domains/firetrucks/firetruck.service";
 import incidentService from "@/data/domains/incident/incident.service";
-
-export async function GET() {
-  const session = await auth();
-
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  const firetruck = await firetruckService.getFiretruckById(
-    session.firetruckId,
-  );
-
-  const incident = await incidentService.getIncidentDetails(
-    firetruck!.activeIncident!,
-  );
-
-  return NextResponse.json({ incident });
-}
 
 export async function POST(request: NextRequest) {
   const session = await auth();
