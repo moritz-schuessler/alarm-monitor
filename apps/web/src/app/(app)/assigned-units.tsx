85d046ab9843bed351a26ab8a2be2ee662e7379e
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-22 01:36:35 +0200

    feat: style assigned-unit component

--- apps/web/src/app/(app)/assigned-units.tsx
+++ apps/web/src/app/(app)/assigned-units.tsx
@@ -7,10 +7,10 @@ interface Props {
 const AssignedUnits = ({ stations }: Props) => {
   return (
     <div className="w-full divide-y-1 divide-border border-1 border-border rounded-lg text-xl col-span-1">
-      <div className="flex flex-col gap-4 p-4">
+      <div className="flex flex-col divide-border divide-y-1">
         {stations.map((station) => {
           return (
-            <div key={station.id} className="flex flex-col">
+            <div key={station.id} className="flex flex-col p-4 gap-1">
               <div>{station.name}</div>
               {station.firetrucks.map((firetruck) => {
                 return (
