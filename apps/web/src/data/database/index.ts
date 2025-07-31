1bf47f9c014c7abf17ff5031a29de9098ba2bac1
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-31 02:59:44 +0200

    chore: remove data-layer from next app

--- apps/web/src/data/database/index.ts
+++
@@ -1,7 +0,0 @@
-import * as schema from "../shared/schema";
-import "@/lib/envConfig";
-import { drizzle } from "drizzle-orm/libsql";
-
-const db = drizzle(process.env.DB_FILE_NAME!, { schema: schema });
-
-export default db;
