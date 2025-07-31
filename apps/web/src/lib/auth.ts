3111f5da7093007ad373c59b4c68a9b1f349d1d6
Moritz Schüßler <mail@moritzschuessler.de>
2025-07-31 02:54:14 +0200

    feat: remove unused auth methods

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,31 +1,10 @@
 import "server-only";
 
 import { cookies } from "next/headers";
-import { createSession, decrypt, deleteSession } from "./session";
-import firetruckService from "@/data/domains/firetrucks/firetruck.service";
 
-const auth = async () => {
+const getSession = async () => {
   const cookie = (await cookies()).get("session")?.value;
-  return await decrypt(cookie);
+  return cookie;
 };
 
-const signIn = async (radioIdentification: string) => {
-  const firetruck =
-    await firetruckService.getFiretruckByRadioIdentification(
-      radioIdentification,
-    );
-
-  if (!firetruck) {
-    return {
-      message: "An error occurred while authenticating.",
-    };
-  }
-
-  await createSession(firetruck.id, firetruck.stationId);
-};
-
-const signOut = async () => {
-  await deleteSession();
-};
-
-export { auth, signIn, signOut };
+export { getSession };
