8359207487f523eca79c0c8a897d9e11c32b5bfa
Moritz Schüßler <mail@moritzschuessler.de>
2025-08-01 21:19:23 +0200

    feat: migrate incidents-endpoint to nestjs

--- apps/backend/nest/src/incidents/incidents.module.ts
+++ apps/backend/nest/src/incidents/incidents.module.ts
@@ -4,10 +4,11 @@ import { IncidentsService } from './incidents.service';
 import { IncidentsRepository } from './incidents.repository';
 import { FiretrucksModule } from 'src/firetrucks/firetrucks.module';
 import { StationsModule } from 'src/stations/stations.module';
+import { IncidentsController } from './incidents.controller';
 
 @Module({
   imports: [DatabaseModule, FiretrucksModule, forwardRef(() => StationsModule)],
-  controllers: [],
+  controllers: [IncidentsController],
   providers: [IncidentsRepository, IncidentsService],
   exports: [IncidentsRepository, IncidentsService],
 })
