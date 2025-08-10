c8d58d7261695c41b68618ef6f205f43db531868
Moritz Schüßler <mail@moritzschuessler.de>
2025-08-10 03:36:31 +0200

    feat(ui): add loading-skeloton to info-card

--- apps/web/src/components/ui/card/info-card.tsx
+++ apps/web/src/components/ui/card/info-card.tsx
@@ -1,10 +1,11 @@
 import { ComponentProps } from "react";
 import { Card, cardVariants } from "./card";
 import { VariantProps } from "class-variance-authority";
+import { Skeleton } from "../skeleton";
 
 type InfoCardProps = {
   description: string;
-  value: string;
+  value: string | null | undefined;
 } & ComponentProps<"div"> &
   VariantProps<typeof cardVariants>;
 
@@ -19,7 +20,9 @@ function InfoCard({
   return (
     <Card status={status} bordered={bordered} className={className} {...props}>
       <div className="text-muted-foreground text-sm">{description}</div>
-      <div className="text-nowrap">{value}</div>
+      <div className="flex justify-center items-center text-nowrap w-full">
+        {value ? value : <Skeleton className="h-[20px] w-1/2 m-1 rounded-sm" />}
+      </div>
     </Card>
   );
 }
