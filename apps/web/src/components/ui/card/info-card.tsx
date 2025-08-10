0887a2508298855ecac23d706632a208fe83d6da
Moritz Schüßler <mail@moritzschuessler.de>
2025-08-10 13:53:16 +0200

    feat(ui): improved PropType of info-card and use ?? as check

--- apps/web/src/components/ui/card/info-card.tsx
+++ apps/web/src/components/ui/card/info-card.tsx
@@ -1,11 +1,11 @@
-import { ComponentProps } from "react";
+import { ComponentProps, ReactNode } from "react";
 import { Card, cardVariants } from "./card";
 import { VariantProps } from "class-variance-authority";
 import { Skeleton } from "../skeleton";
 
 type InfoCardProps = {
   description: string;
-  value: string | null | undefined;
+  value: ReactNode;
 } & ComponentProps<"div"> &
   VariantProps<typeof cardVariants>;
 
@@ -21,7 +21,7 @@ function InfoCard({
     <Card status={status} bordered={bordered} className={className} {...props}>
       <div className="text-muted-foreground text-sm">{description}</div>
       <div className="flex justify-center items-center text-nowrap w-full">
-        {value ? value : <Skeleton className="h-[20px] w-1/2 m-1 rounded-sm" />}
+        {value ?? <Skeleton className="h-[20px] w-1/2 m-1 rounded-sm" />}
       </div>
     </Card>
   );
