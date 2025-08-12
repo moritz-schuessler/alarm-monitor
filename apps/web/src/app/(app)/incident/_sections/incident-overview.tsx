5cfd37d26aad6075a013e970e5f4d38df61f24f2
Moritz Schüßler <mail@moritzschuessler.de>
2025-08-13 01:33:50 +0200

    feat: show skills in incident view

--- apps/web/src/app/(app)/incident/_sections/incident-overview.tsx
+++ apps/web/src/app/(app)/incident/_sections/incident-overview.tsx
@@ -1,6 +1,26 @@
 import { StatCard } from "@/components/ui/card/stat-card";
+import useIncident from "@/hooks/use-get-incident";
+import { FirefighterDetails } from "@alarm-monitor/shared/src";
 
 const IncidentOverview = () => {
+  const { data: incident } = useIncident();
+
+  const firefighters = incident
+    ? incident?.firetrucks.flatMap((truck) => truck.crew.firefighters)
+    : [];
+
+  const agt = firefighterQuery(firefighters)
+    .filterByQualification("Atemschutzgeräteträger")
+    .toArray();
+
+  const fgAgt = firefighterQuery(firefighters)
+    .filterByQualification("Fachgebiet Atemschutz")
+    .toArray();
+
+  const presse = firefighterQuery(firefighters)
+    .filterByQualification("Fachgebiet Presse")
+    .toArray();
+
   return (
     <div className="grid grid-cols-3 gap-0.25 ring ring-border h-full auto-rows-[auto_1fr] ">
       <div className="grid grid-cols-subgrid col-span-3 gap-0.25">
@@ -8,11 +28,52 @@ const IncidentOverview = () => {
           Einsatzstatisiken
         </div>
       </div>
-      <div className="grid grid-cols-subgrid col-span-3 h-full">
-        <StatCard description="Placeholder" value={12} />
+      <div className="grid grid-cols-subgrid col-span-3 h-full bg-secondary">
+        <StatCard
+          description="Einsatzkräfte"
+          value={firefighters.length}
+          className="bg-background"
+        />
+        <StatCard
+          description="AGT"
+          value={agt.length}
+          className="bg-background"
+        />
+        <StatCard
+          description="Fachgebiet Atemschutz"
+          value={fgAgt.length}
+          className="bg-background"
+        />
+        <StatCard
+          description="Fachgebiet Presse"
+          value={presse.length}
+          className="bg-background"
+        />
       </div>
     </div>
   );
 };
+
+function firefighterQuery(list: FirefighterDetails[]) {
+  return {
+    list,
+    filterByQualification(name: string) {
+      return firefighterQuery(
+        list.filter((f) =>
+          f.qualificationToFirefighter.some(
+            (q) => q.qualification.name === name,
+          ),
+        ),
+      );
+    },
+    filterByFirefighters(allowed: FirefighterDetails[]) {
+      const allowedIds = new Set(allowed.map((f) => f.id));
+      return firefighterQuery(list.filter((f) => allowedIds.has(f.id)));
+    },
+    toArray() {
+      return list;
+    },
+  };
+}
 
 export default IncidentOverview;
