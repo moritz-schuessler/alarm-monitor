faf0698cc65428e19f76a7a37df78e366ee18f34
Moritz Schüßler <mail@moritzschuessler.de>
2025-08-14 15:09:55 +0200

    feat: display badges for a set of qualifications

--- apps/web/src/app/(app)/incident/_components/crew.tsx
+++ apps/web/src/app/(app)/incident/_components/crew.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -120,8 +121,13 @@ const Seat = (props: SeatProps) => {
   }
 
   if (props.type === "firefighter") {
+    const qualificationsToFirefighter =
+      props.firefighter.qualificationToFirefighter;
+
+    const badges = filterQualifications(qualificationsToFirefighter);
+
     return (
-      <div className="flex flex-col ring ring-border gap-0.25 bg-background">
+      <div className="flex flex-col ring ring-border gap-0.25 bg-background justify-between">
         <div className="flex justify-between">
           <div className="flex items-center p-4 w-full">
             {props.firefighter.name}
@@ -139,9 +145,103 @@ const Seat = (props: SeatProps) => {
             <UserRoundMinus className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
           </Button>
         </div>
+        <div className="flex flex-wrap p-4 gap-2">
+          {badges.map((qualificationToFirefighter) => {
+            return (
+              <Badge
+                key={
+                  qualificationToFirefighter?.qualification.id +
+                  props.firefighter.id
+                }
+                variant={
+                  qualificationToFirefighter?.status === "active"
+                    ? "secondary"
+                    : "destructive"
+                }
+                className="text-md"
+              >
+                {qualificationToFirefighter?.qualification.name}
+              </Badge>
+            );
+          })}
+        </div>
       </div>
     );
   }
+};
+
+const filterQualifications = (
+  qualificationToFirefighter: FirefighterDetails["qualificationToFirefighter"],
+) => {
+  const filteredQualifications = [];
+
+  const findActiveQualification = (name: string) =>
+    qualificationToFirefighter.find(
+      (q) => q.qualification.name === name && q.status === "active",
+    );
+
+  const leadership = {
+    verbandsfuehrer: findActiveQualification("Verbandsführer"),
+    zugfuehrer: findActiveQualification("Zugführer"),
+    gruppenfuehrer: findActiveQualification("Gruppenführer"),
+    truppfuehrer: findActiveQualification("Truppführer"),
+    grundlehrgang: findActiveQualification("Grundlehrgang"),
+  };
+
+  if (leadership.grundlehrgang) {
+    const highestLeadership =
+      leadership.verbandsfuehrer ??
+      leadership.zugfuehrer ??
+      leadership.gruppenfuehrer ??
+      leadership.truppfuehrer ??
+      leadership.grundlehrgang;
+
+    filteredQualifications.push(highestLeadership);
+  }
+
+  const agt = findActiveQualification("Atemschutzgeräteträger");
+
+  if (agt) {
+    const AGT_REQUIREMENTS = [
+      "Atemschutz/Atemschutzstrecke",
+      "Atemschutz/Unterweisung",
+      "Atemschutz/Einsatz/Übung",
+      "G26.3",
+    ];
+
+    const activeRequirements = AGT_REQUIREMENTS.filter((reqName) =>
+      findActiveQualification(reqName),
+    );
+
+    const agtWithStatus = {
+      ...agt,
+      status:
+        activeRequirements.length === AGT_REQUIREMENTS.length
+          ? "active"
+          : "expired",
+    };
+
+    filteredQualifications.push(agtWithStatus);
+  }
+
+  const maschinist = findActiveQualification("Maschinist");
+  if (maschinist) {
+    filteredQualifications.push(maschinist);
+  }
+
+  const drivingLicenses = {
+    ce: findActiveQualification("CE"),
+    c: findActiveQualification("C"),
+    feuerwehr: findActiveQualification("Feuerwehrführerschein"),
+  };
+
+  const highestLicense =
+    drivingLicenses.ce ?? drivingLicenses.c ?? drivingLicenses.feuerwehr;
+  if (highestLicense) {
+    filteredQualifications.push(highestLicense);
+  }
+
+  return filteredQualifications;
 };
 
 export default Crew;
